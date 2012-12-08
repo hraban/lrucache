ec93163f07a464da84434f94c079632d12590240
Hraban Luyat <hraban@0brg.net>
2012-12-09 01:46:41 +0200

    Much simpler implementation of ThrottleConcurrency
    Unfortunately not as memory efficient (O(n) vs O(1)) but I think it is
    totally worth it. Also 58008.

--- limitconcurrency.go
+++ limitconcurrency.go
@@ -1,9 +1,5 @@
 package lrucache
 
-import (
-	"sync"
-)
-
 // Process operations concurrently except for those with an identical key.
 func nocondupesMainloop(f func(string) Cacheable, opchan chan reqGet) {
 	// Push result of call to wrapped function down this channel
@@ -55,24 +51,11 @@ func NoConcurrentDupes(f func(string) Cacheable) func(string) Cacheable {
 // Wrapper function that limits the number of concurrent calls to f. Intended
 // for wrapping OnMiss handlers.
 func ThrottleConcurrency(f func(string) Cacheable, maxconcurrent uint) func(string) Cacheable {
-	c := sync.NewCond(&sync.Mutex{})
-	n := 0
+	block := make(chan int, maxconcurrent)
 	return func(key string) Cacheable {
-		c.L.Lock()
-		// See, if I put n += 1 above this for loop and check for > only,
-		// suddenly no more than maxconcurrent calls are ever executed at all.
-		// I really do not understand why that is, which is not a good sign.
-		// What is going on?
-		for n >= int(maxconcurrent) {
-			c.Wait()
-		}
-		n += 1
-		c.L.Unlock()
+		block <- 58008
 		res := f(key)
-		c.L.Lock()
-		n -= 1
-		c.L.Unlock()
-		c.Signal()
+		<-block
 		return res
 	}
 }
